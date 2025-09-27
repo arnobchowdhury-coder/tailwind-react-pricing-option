5650d5142b7108d082e4f90fd5eff8983100084f
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-09-27 19:48:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import DaisyNav from './Component/DaisyNav/DaisyNav'
+import NavBar from './Component/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+
+      <header>
+        <NavBar></NavBar>
+        <DaisyNav></DaisyNav>
+      </header>
+
+
+
+
+    </>
+  )
+}
+
+export default App
