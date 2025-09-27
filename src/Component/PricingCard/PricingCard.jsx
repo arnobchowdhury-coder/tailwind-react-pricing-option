423500e918bb77ba55d465360b0e3cbb8efdb233
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-09-27 21:21:57 +0600

    added some

--- src/Component/PricingCard/PricingCard.jsx
+++ src/Component/PricingCard/PricingCard.jsx
@@ -5,19 +5,20 @@ const PricingCard = ({ pricing }) => {
   const { name, price, info, features } = pricing;
 
   return (
-    <div className='border bg-amber-300 rounded-2xl p-4'>
+    <div className='flex flex-col border bg-amber-300 rounded-2xl p-4'>
       {/* Card header */}
       <div>
         <h1 className="text-7xl">{name}</h1>
         <h1 className="text-3xl">{price}</h1>
       </div>
       {/* Card body */}
-      <div className='bg-amber-500 p-4 rounded-2xl mt-10'>
+      <div className='bg-amber-500 p-4 rounded-2xl mt-10 flex-1'>
         <p>{info}</p>
         {
           features.map((feature, index) => <PricingFeatures key={index} feature={feature}></PricingFeatures>)
         }
       </div>
+      <button className="btn w-full mt-4">Subscribe</button>
     </div>
   );
 };
--- src/Component/PricingOptions/PricingOptions.jsx
+++ src/Component/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricing from '../DaisyPricing/DaisyPricing';
 
 const PricingOptions = ({ pricingPromise }) => {
 
@@ -9,9 +10,17 @@ const PricingOptions = ({ pricingPromise }) => {
   return (
     <div>
       <h2 className='text-4xl'>Get Our MemberShip</h2>
-      <div className='grid md:grid-cols-3 gap-8'>
-        {
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {/* {
           pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+        } */}
+
+        {
+          pricingData.map(pricing => <DaisyPricing
+            key={pricing.id}
+            pricing={pricing}
+          >
+          </DaisyPricing>)
         }
       </div>
     </div>
